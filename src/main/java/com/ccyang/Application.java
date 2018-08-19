351f02378afdd8cbeca637f6de3f6a42fa688089
yangjinpeng <1415946318@qq.com>
2018-08-19 19:10:22 +0800

    项目初始化完成

---
+++ src/main/java/com/ccyang/Application.java
@@ -0,0 +1,13 @@
+package com.ccyang;
+
+/**
+ * Hello world!
+ *
+ */
+public class Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
