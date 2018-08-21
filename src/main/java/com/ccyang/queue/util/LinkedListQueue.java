6f68e47c787b3363d5599581b1da7b1f90dd87a8
yangjinpeng <1415946318@qq.com>
2018-08-21 23:04:59 +0800

    使用链表实现队列

---
+++ src/main/java/com/ccyang/queue/util/LinkedListQueue.java
@@ -0,0 +1,117 @@
+package com.ccyang.queue.util;
+
+
+import com.ccyang.linkedList.util.LinkedList;
+
+/**
+ * @author ccyang
+ * @date 2018/8/21 22:49
+ */
+public class LinkedListQueue<E> implements Queue<E> {
+
+    private class Node{
+
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next){
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e){
+            this.e = e;
+            this.next = null;
+        }
+
+        public Node(){
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+
+    }
+
+    private Node head, tail;
+    private int size;
+
+    public LinkedListQueue(){
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if(tail == null) {  // tail == null 可以推断出 head == null
+            tail = new Node(e);
+            head = tail;
+        }else{
+            tail.next = new Node(e);
+            tail = tail.next;
+        }
+        size ++;
+    }
+
+    @Override
+    public E dequeue() {
+        if(isEmpty())
+            throw new IllegalArgumentException("cannot dequeue from an empty queue.");
+
+        Node retNode = head;
+        head = head.next;
+        retNode.next = null;
+        if(head == null)
+            tail = null;
+        size --;
+
+        return retNode.e;
+    }
+
+    @Override
+    public E getFront() {
+        if(isEmpty())
+            throw new IllegalArgumentException("cannot dequeue from an empty queue.");
+        return head.e;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue： front ");
+        Node cur = head;
+        while(cur != null){
+            res.append(cur + "->");
+            cur = cur.next;
+        }
+        res.append("NULL tail");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        LinkedListQueue<Integer> queue = new LinkedListQueue<>();
+        for (int i = 0; i < 10 ; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+
+            if(i % 3 == 2){
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
