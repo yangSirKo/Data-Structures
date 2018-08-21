948f922807261b3df0026d701aa915b28a795f41
yangjinpeng <1415946318@qq.com>
2018-08-21 23:08:50 +0800

    比较链表实现栈和数组实现栈

---
+++ src/main/java/com/ccyang/stack/util/Main.java
@@ -0,0 +1,45 @@
+package com.ccyang.stack.util;
+
+import com.ccyang.linkedList.util.ArrayStack;
+import com.ccyang.linkedList.util.LinkedListStack;
+import com.ccyang.linkedList.util.Stack;
+
+import java.util.Random;
+
+/**
+ * @author ccyang
+ * @date 2018/8/21 21:56
+ */
+public class Main {
+
+    private static double testStack(Stack<Integer> stack, int opCount){
+
+        long startTime = System.nanoTime();
+        Random random = new Random();
+        for (int i=0; i<opCount; i++)
+            stack.push(random.nextInt(Integer.MAX_VALUE));
+
+        for (int i=0; i<opCount; i++)
+            stack.pop();
+
+        long endTime = System.nanoTime();
+        return (endTime - startTime) / 1000000000.0 ;
+    }
+
+    public static void main(String[] args) {
+
+        int opCount = 100000;
+
+        ArrayStack<Integer> arrayStack = new ArrayStack<>();
+        double time1 = testStack(arrayStack, opCount);
+        System.out.println("ArrayStack: time1 = " + time1 +"s");  // 0.0373s
+
+        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
+        double time2 = testStack(linkedListStack, opCount);
+        System.out.println("LinkedListStack: time2 = " + time2 +"s");  // 0.0154s
+
+        // 这个比较很复杂，因为LinkedListStack 中包含更多的 new操作
+
+    }
+
+}
